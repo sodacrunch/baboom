b0bcf15f5bc9500ec017a0ef5998adfafe25bd6e
Haroon Khalid <haroon@sodacrunch.com>
2013-06-27 17:07:38 -0400

    First commit

---
+++ game.py
@@ -0,0 +1,17 @@
+# game.py
+# A game where you have to catch all the items
+# Haroon Khalid
+
+import pygame
+import random
+import os
+from sys import exit
+
+import setup
+
+pygame.init()
+
+new_game = setup.Setup()
+new_game.run()
+
+
